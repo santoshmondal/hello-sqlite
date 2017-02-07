35fa87771f9c5ca88ce2ddcc0b5228f58c5ece0c
santosh <santosh.ece06@gmail.com>
2017-02-07 17:06:03 +0530

    structure.

--- secured/xsimulation/sample7.js
+++ secured/xsimulation/sample7.js
@@ -9,10 +9,25 @@ db.serialize(function(){
     dbManager.execDML();
 
     var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+
+    /* PARAM AS ARGUMENT REFERENCE */
     for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
+        stmt.run("PARAM_SIMPLE " + i);
+    }
+
+    /* PARAM ARRAY REFERENCE */
+    for (var i = 0; i < 10; i++) {
+        stmt.run(["PARAM_AS_ARRAY " + i]);
     }
     stmt.finalize();
+
+    /* NAMED PARAM REFERENCE */
+    var stmt = db.prepare("INSERT INTO user_info VALUES ($info)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run({$info : "NAMED_PARAM"});
+    }
+    stmt.finalize();
+
 
     db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
         console.log(row.id + ": " + row.info);
